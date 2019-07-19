5de1e97b5c00dcbc5fa61201e3474c3c4e135c1a
cakazies <cakazies@gmail.com>
2019-07-19 13:02:46 +0700

    add command

--- bbc.com/main.go
+++ bbc.com/main.go
@@ -1,3 +1,6 @@
+/*
+ * this package for get data from scrapping BBC without unit testing , because , this art just for improve and learn
+ */
 package main
 
 import (
@@ -5,22 +8,26 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery" // library for scrapping
 )
 
+// struct for representatif for data when we put
 type Article struct {
-	Title   string `json:"title,omitempty"`
-	Content string `json:"content,omitempty"`
-	Link    string `json:"link,omitempty"`
+	Title   string `json:"title,omitempty"`   // title struct for add.in tittle in this scrapping
+	Content string `json:"content,omitempty"` // content struct for add.in content in this scrapping
 }
 
+// function main for run this file
 func main() {
-	ScrapeHTML()
+	ScrapeHTML() // call function in this file
 }
 
 //ScrapeHTML is function to scrape
 func ScrapeHTML() {
-	res, err := http.Get("https://www.bbc.com/indonesia/indonesia?xtor=SEC-3-[GOO]-[70466280620]-[346260440805]-S-[berita%20internasional]")
+	// setting your target website get scrapping
+	website := "https://www.bbc.com/indonesia/indonesia?xtor=SEC-3-[GOO]-[70466280620]-[346260440805]-S-[berita%20internasional]"
+	// connect the website
+	res, err := http.Get(website)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,22 +36,26 @@ func ScrapeHTML() {
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
+	// call function to get all HTML in this website
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// make array struct for saving data
 	rows := make([]Article, 0)
-
+	// call class including all data tittle and content in this website
 	doc.Find(".eagle").Children().Each(func(i int, sel *goquery.Selection) {
+		// call this struct and representatif
 		row := new(Article)
 		row.Title = sel.Find(".title-link__title-text").Text()
 		row.Content = sel.Find(".eagle-item__summary").Text()
 		rows = append(rows, *row)
 	})
-
+	// change data struct in json
 	bts, err := json.MarshalIndent(rows, "", "  ")
 	if err != nil {
 		log.Fatal(err)
 	}
+	// print to json
 	log.Println(string(bts))
 }
